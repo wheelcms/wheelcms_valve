424aea1471129fd35b283a8f8f54224822bf9b55
Ivo van der Wijk <github@in.m3r.nl>
2013-04-18 15:57:25 +0200

    styling; refactoring sectionedpaginator

--- wheelcms_valve/models.py
+++ wheelcms_valve/models.py
@@ -1,4 +1,5 @@
-from django.core.paginator import Paginator, InvalidPage
+from django.core.paginator import InvalidPage
+from two.bootstrap.paginator import SectionedPaginator
 
 from wheelcms_axle.content import type_registry
 from wheelcms_axle.templates import template_registry
@@ -28,37 +29,6 @@ class ValveEntryType(PageType):
 class ValveBlog(PageBase):
     pass
 
-class SectionedPaginator(Paginator):
-    def sections(self, page, windowsize=6):
-        np = self.num_pages
-
-        b = m = e = []
-
-        ##
-        ## provide sort of a sliding window over the available
-        ## pages with the current page in the center.
-        ## windowsize is the size of this window
-        if np > windowsize*2:
-            b = [1]
-            e = [np]
-            start = max(3, page - windowsize/2)
-            end = start + windowsize+1
-            if end > np-2:
-                end = np - 1
-                start = end - windowsize - 1
-
-            if page < 4:
-                b = range(1, windowsize)
-                e = [np]
-            elif page > (np - 3):
-                b = [1]
-                e = range(np+1-windowsize, np+1)
-            else:
-                m = range(start, end)
-        else:
-            b = range(1, np + 1)
-
-        return b, m, e
 
 class ValveBlogType(PageType):
     model = ValveBlog
