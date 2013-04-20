56c5c428b5f3921d775bc631fcafa4f5ea41888a
Ivo van der Wijk <github@in.m3r.nl>
2013-04-20 18:24:34 +0200

    provide (frontpage) Page template

--- wheelcms_valve/models.py
+++ wheelcms_valve/models.py
@@ -2,6 +2,7 @@ from django.core.paginator import InvalidPage
 from two.bootstrap.paginator import SectionedPaginator
 
 from wheelcms_axle.content import type_registry
+from wheelcms_axle.node import Node
 from wheelcms_axle.templates import template_registry
 
 from wheelcms_spokes.page import PageBase, PageType, PageForm
@@ -29,6 +30,30 @@ class ValveEntryType(PageType):
 class ValveBlog(PageBase):
     pass
 
+def blog_context(handler, request, node):
+    """ provide context both in Page as in Blog context """
+    ctx = {}
+
+    ctx['body_class'] = "wheelcms_valve"
+
+    p = max(1, int(request.GET.get('page', 1)))
+    kw = {}
+    if not handler.hasaccess():
+        kw['contentbase__state'] = "published"
+
+    ctx['paginator'] = paginator = SectionedPaginator(Node.objects.filter(path__startswith=node.path + '/', contentbase__meta_type=ValveEntry.classname, **kw).order_by("-contentbase__created"), 4)
+    # ctx['paginator'] = paginator = SectionedPaginator(node.childrenq(contentbase__meta_type=ValveEntry.classname, **kw).order_by("-contentbase__created"), 4)
+    b, m, e = paginator.sections(p, windowsize=6)
+    ctx['begin'] = b
+    ctx['middle'] = m
+    ctx['end'] = e
+
+    try:
+        ctx['page'] = paginator.page(p)
+    except InvalidPage:
+        ctx['page'] = paginator.page(1)
+
+    return ctx
 
 class ValveBlogType(PageType):
     model = ValveBlog
@@ -44,23 +69,7 @@ class ValveBlogType(PageType):
 
     def context(self, handler, request, node):
         ctx = super(ValveBlogType, self).context(handler, request, node)
-
-
-        p = max(1, int(request.GET.get('page', 1)))
-        kw = {}
-        if not handler.hasaccess():
-            kw['contentbase__state'] = "published"
-
-        ctx['paginator'] = paginator = SectionedPaginator(self.instance.node.childrenq(contentbase__meta_type=ValveEntry.classname, **kw).order_by("-contentbase__created"), 4)
-        b, m, e = paginator.sections(p, windowsize=6)
-        ctx['begin'] = b
-        ctx['middle'] = m
-        ctx['end'] = e
-
-        try:
-            ctx['page'] = paginator.page(p)
-        except InvalidPage:
-            ctx['page'] = paginator.page(1)
+        ctx.update(blog_context(handler, request, node))
 
         return ctx
 
@@ -70,3 +79,7 @@ template_registry.register(ValveBlogType, "wheelcms_valve/valveblog_view.html",
 type_registry.register(ValveEntryType)
 template_registry.register(ValveEntryType, "wheelcms_valve/valveentry_view.html",
                            "Blog entry view", default=True)
+
+template_registry.register(PageType, "wheelcms_valve/valveblog_view.html",
+                           "Blog view", default=False, context=blog_context)
+
