9541b218fd21c30ea464623023cae1492367843d
Ivo van der Wijk <github@in.m3r.nl>
2013-06-30 17:22:46 +0200

    travis ci support

---
+++ quicktest.py
@@ -0,0 +1,37 @@
+from twotest.quicktest import QuickDjangoTest
+
+if __name__ == '__main__':
+    QuickDjangoTest(
+        apps=("wheelcms_valve",),
+        installed_apps=(
+            ## these are mostly wheelcms_axle's dependencies
+            'django.contrib.sites',
+            'django.contrib.messages',
+            'django.contrib.staticfiles',
+            'django.contrib.humanize',
+
+            'south',
+            'haystack',
+
+            'wheelcms_axle',
+            'wheelcms_spokes',
+            'wheelcms_categories',
+            'wheelcms_valve',
+
+            'taggit',
+            'two.ol',
+            'two.bootstrap',
+            'twotest',
+            'wheelcms_axle.tests',
+
+        ),
+        ROOT_URLCONF="wheelcms_axle.test_urls",
+        ANONYMOUS_USER_ID=-1,
+        HAYSTACK_SITECONF = 'wheelcms_axle.search_sites',
+        HAYSTACK_SEARCH_ENGINE = 'simple',
+        AUTH_PROFILE_MODULE="wheelcms_axle.WheelProfile",
+        CLEANUP_MEDIA=True,
+        TEST_MEDIA_ROOT="/tmp/wheelcms_axle_test",
+        USE_TZ=True,
+        STATIC_URL='/',
+    )
